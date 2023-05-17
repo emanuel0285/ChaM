834b6330f6963c4200d8b1c25518db47fe133c67
emanuel0285 <111258490+emanuel0285@users.noreply.github.com>
2023-05-16 22:27:32 -0400

    Update chat2.py

--- chat2.py
+++ chat2.py
@@ -92,17 +92,8 @@ def run_chatbot():
                 generate_report()
                 conversation.append((question, "Report Generated"))
             else:
-                answer = get_answer(question)
-                if answer is None:
                     st.write("Sorry, I don't understand. Do you need help? Type 'help' or 'h' for more information. Type 'exit' or 'e' to quit.")
-                    conversation.append((question, "Unknown"))
-                elif answer == "Not Found":
-                    st.write("Sorry, the question you asked is not in my database. Do you need help? Type 'help' or 'h' for more information. Type 'exit' or 'e' to quit.")
-                    conversation.append((question, "Not Found"))
-                else:
-                    st.write(answer)
-                    conversation.append((question, answer))
-
+                   
     with open("chatbot_conversation.txt", "w") as file:
         for q, a in conversation:
             file.write(f"{q}\t{a}\n")
